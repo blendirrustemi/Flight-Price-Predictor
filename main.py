707acee407642838770517bf2706101ee15b57b1
Blendi Rrustemi <blendirrustemi9@gmail.com>
2023-04-13 15:50:13 +0200

    Removed null values, converted Stops to integer

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-import csv
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
 
-data_df = pd.read_csv('OtherData/Data_train.csv')
+data_df = pd.read_csv('Datasets/Data_train_id.csv')
 
-print(data_df['Route'])
+print(data_df.head())
+
+
+# check for missing values
+print("Missing values: ", data_df.isnull().sum().sum())
+
+
+# remove missing values
+data_df = data_df.dropna()
+
+
+# check and remove for duplicates
+print("Duplicates: ", data_df.duplicated().sum())
+
+
+# convert the Total_Stops column from "non-stop", "1 stop, "2 stops", etc. to 0, 1, 2, etc
+# first, check the number of stops
+print(data_df['Total_Stops'].value_counts())
+
+data_df['Total_Stops'] = data_df['Total_Stops'].replace({'non-stop': 0, '1 stop': 1, '2 stops': 2,
+                                                         '3 stops': 3, '4 stops': 4})
+
